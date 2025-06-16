b06dc54860cf3ce634767386fe4a63af0faeb3fa
Jesse Bremer <jessesrbremer@gmail.com>
2025-06-16 12:26:06 -0400

    Imported art, restarts program if prompted and user agrees, otherwise terminates.

--- Caesar Cipher 3/main.py
+++ Caesar Cipher 3/main.py
@@ -1,10 +1,10 @@
-# TODO-1: Import and print the logo from art.py when the program starts.
-
+from art import logo
+print(logo)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+run_program = "yes"
 
 # TODO-2: What happens if the user enters a number/symbol/space?
-
 
 def caesar(original_text, shift_amount, encode_or_decode):
     output_text = ""
@@ -19,14 +19,23 @@ def caesar(original_text, shift_amount, encode_or_decode):
     print(f"Here is the {encode_or_decode}d result: {output_text}")
 
 
-# TODO-3: Can you figure out a way to restart the cipher program?
 
+while run_program == "yes":
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+    while True:
+        run_program = input("Use the service again? 'Yes' or 'No' \n").strip().lower()
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+        if run_program == "yes":
+            break
+        elif run_program == "no":
+            print("Goodbye.")
+            exit()
+        else:
+            print("Invalid input. Please type 'Yes' or 'No'.")
 
-caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
 
 
 

7421567050d42afa98554eddd2874e13fa034b7c
Jesse Bremer <jessesrbremer@gmail.com>
2025-06-16 10:22:06 -0400

    Multiple Input Function Parameters.

--- Positional vs Keyword Arguments/task.py
+++ Positional vs Keyword Arguments/task.py
@@ -1,18 +1,17 @@
 # Functions with input
-def life_in_weeks(current_age):
-    weeks = 52
-    amount_of_weeks_90 = weeks * 90
-    weeks_already_lived = current_age * weeks
-    weeks_left = amount_of_weeks_90 - weeks_already_lived
-    print(f"You have {weeks_left} weeks left.")
-
-
-life_in_weeks(32)
-
-
-# def greet_with_name(name):
-#     print(f"Hello {name}")
-#     print(f"How do you do {name}?")
+# def life_in_weeks(current_age):
+#     weeks = 52
+#     amount_of_weeks_90 = weeks * 90
+#     weeks_already_lived = current_age * weeks
+#     weeks_left = amount_of_weeks_90 - weeks_already_lived
+#     print(f"You have {weeks_left} weeks left.")
 #
 #
-# greet_with_name("Jack Bauer")
+# life_in_weeks(32)
+
+def greet(name, location):
+    print(f"Hello {name}")
+    print(f"You live in {location}")
+
+
+greet("Jack Bauer", "Ottawa")

dd21e773b39337d80878a11be80433a25d52fa05
Jesse Bremer <jessesrbremer@gmail.com>
2025-06-16 11:36:49 -0400

    Setup shifting and encoding. adjusting for edge case where if the alphabet array ends, to loop back to the start using a modula operator.

--- Caesar Cipher 1/main.py
+++ Caesar Cipher 1/main.py
@@ -5,13 +5,15 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-# TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
+def encrypt(original_text, shift_amount):
+    encoded_message = ""
+    for char in original_text:
+        index_value = alphabet.index(char)
+        new_index = (index_value + shift_amount) % len(alphabet)
+        encoded_message += alphabet[new_index]
 
-# TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
-#  by the shift amount and print the encrypted text.
+    print(encoded_message)
+    return encoded_message
 
-# TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
-
-# TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
-#  message.
+encrypt(text, shift)
 
